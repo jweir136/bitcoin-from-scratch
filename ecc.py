4a97d27134402dff8504cd02041db32d66f88def
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-20 17:27:41 -0500

    implement der method for Signature

--- ecc.py
+++ ecc.py
@@ -194,6 +194,19 @@ class Signature:
     def __repr__(self):
         return 'Signature({:x}, {:x})'.format(self.r, self.s)
 
+    def der(self):
+        rbin = self.r.to_bytes(32, byteorder='big')
+        rbin = rbin.lstrip(b'\x00')
+        if rbin[0] & 0x80:
+            rbin = b'\x00' + rbin
+        result = bytes([2, len(rbin)]) + rbin
+        sbin = self.s.to_bytes(32, byteorder='big')
+        sbin = sbin.lstrip(b'\x00')
+        if sbin[0] & 0x80:
+            sbin = b'\x00' + sbin
+        result += bytes([2, len(sbin)]) + sbin
+        return bytes([0x30, len(result)]) + result
+
 class PrivateKey:
 
     def __init__(self, secret):
