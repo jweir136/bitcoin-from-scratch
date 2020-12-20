3a6d805d8cd429cc98819ed48d968b944e53673a
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-20 09:42:32 -0500

    add inverse case for __add__ at ecc.Point

--- ecc.py
+++ ecc.py
@@ -66,9 +66,22 @@ class Point:
 
         if self.y**2 != self.x**3 + a * x + b:
             raise ValueError("({}, {}) is not on the curve".format(x, y))
+        if self.x is None and self.y is None:
+            return
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y and self.a == other.a and self.b == other.b
 
     def __ne__(self, other):
         return self.x != other.x and self.y != other.y and self.y != other.a and self.b != other.b
+
+    def __add__(self, other):
+        if self.a != other.a or self.b != other.b:
+            raise TypeError("Points {}, {} are not on the same curve".format(self, other))
+        if self.x is None:
+            return other
+        elif other.x is None:
+            return self
+
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None, None, self.a, self.b)
