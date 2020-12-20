6bbad58f0ed5cf40d186f13d5142cd29f6376741
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-20 14:35:36 -0500

    implement verify for S256Point

--- ecc.py
+++ ecc.py
@@ -141,6 +141,13 @@ class S256Point(Point):
         coef = coef % N
         return super().__rmul__(coef)
 
+    def verify(self, z, sig):
+        s_inv = pow(sig.s, N - 2, N)
+        u = z * s_inv % N
+        v = sig.r * s_inv % N
+        total = u * G + v * self
+        return total.x.num == sig.r
+
 G = S256Point(
     0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
     0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
