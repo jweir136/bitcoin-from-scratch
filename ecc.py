be58be62ed517f42bfa49f789a0fbbfadd2faecc
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-20 14:09:56 -0500

    init S256Field

--- ecc.py
+++ ecc.py
@@ -114,6 +114,16 @@ class Point:
             coef >>= 1
         return result
 
+P = 2**256 - 2**32 - 977
+
+class S256Field(FieldElement):
+
+    def __init__(self, num, prime=None):
+        super().__init__(num=num, prime=P)
+
+    def __repr__(self):
+        return "{:x}".format(self.num).zfill(64)
+
 class ECCTest():
     
     def test_on_curve(self):
